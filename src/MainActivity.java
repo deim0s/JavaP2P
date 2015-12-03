697e533b809c4ded0054f2a6593ee02f42f0e24d
deim0s <atymick@gmail.com>
2015-12-03 01:15:42 -0600

    added files

---
+++ src/MainActivity.java
@@ -0,0 +1,24 @@
+import javax.swing.UIManager;
+/**
+ *
+ * @author Alex
+ */
+public class MainActivity {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(MainFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new MainFrame().setVisible(true);
+            }
+        });
+    }
+}
